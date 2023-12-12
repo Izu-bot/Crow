62e37c7ef88248b32163988b3021bda0983f811a
ZxwQ <kauanmartins977@gmail.com>
2023-12-12 08:32:24 -0300

    Login page

--- OnlineStore/src/assets/routes/Conta.jsx
+++ OnlineStore/src/assets/routes/Conta.jsx
@@ -1,19 +1,21 @@
+import { Link } from 'react-router-dom';
+
 import '../styles/Conta.css';
 
 export default function Conta() {
     return (
         <div className="container">
             <h1 className='titulo'>Login</h1>
-                <form className="form-container">
-                    <div className="form-dados">
-                        <input type="email" autoFocus placeholder='Email' />
-                        <input type="password" placeholder='Senha' />
-                    </div>
-                    <div className="form-btn">
-                        <button type='button'>Voltar</button>
-                        <button type="submit">Entrar</button>
-                    </div>
-                </form>
+            <form className="form-container" action='' method='POST'>
+                <div className="form-dados">
+                    <input type="email" autoFocus placeholder='Email' />
+                    <input type="password" placeholder='Senha' />
+                </div>
+                <div className="form-btn">
+                    <button type="submit">Entrar</button>
+                    <Link to="criarConta">Criar conta</Link>
+                </div>
+            </form>
         </div>
     )
 }
