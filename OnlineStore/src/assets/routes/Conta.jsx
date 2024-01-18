82d8d7c5db9626f2ef9363ffc10d9f5bde4cf01d
ZxwQ <kauanmartins977@gmail.com>
2024-01-18 10:17:21 -0300

    autent usuario e token

--- OnlineStore/src/assets/routes/Conta.jsx
+++ OnlineStore/src/assets/routes/Conta.jsx
@@ -1,15 +1,49 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react'
 
 import '../styles/Conta.css';
 
 export default function Conta() {
+
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        
+        const response = await fetch('http://localhost:3333/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({email, senha}),
+        })
+
+        if(response.ok) {
+            // autenticação bem sucedida
+            const data = await response.json()
+
+            localStorage.setItem('token', data.token)
+
+            console.log('Login bem-sucedido:')
+            // redirecionar
+            navigate("/")
+            
+        } else {
+            // exibir msg de erro
+            console.error(`Erro ao realizar login`, response.statusText)
+        }
+    }
+
     return (
         <div className="container">
             <h1 className='titulo'>Login</h1>
-            <form className="form-container" action='' method='POST'>
+            <form className="form-container" method='POST' onSubmit={handleSubmit}>
                 <fieldset className="form-dados">
-                    <input type="email" autoFocus placeholder='Email' />
-                    <input type="password" placeholder='Senha' />
+                    <input type="email" autoFocus placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" placeholder='Senha' value={senha} onChange={(e) => setSenha(e.target.value)}/>
                 </fieldset>
                 <fieldset className="form-btn">
                     <button type="submit">Entrar</button>
--- OnlineStore/src/assets/routes/CriarConta.jsx
+++ OnlineStore/src/assets/routes/CriarConta.jsx
@@ -1,36 +1,89 @@
 import { Link } from "react-router-dom"
+import { useState } from "react";
 
 import '../styles/CriarConta.css';
 
 export default function CriarConta() {
+
+    const [dadosFormulario, setDadosFormulario] = useState({
+        name: '',
+        surname: '',
+        address: '',
+        cpf: '',
+        date: '',
+        email: '',
+        senha: '',
+    })
+
+    const handleChange = (campo, valor) => {
+        setDadosFormulario({ ...dadosFormulario, [campo]: valor })
+    }
+
+    const handleLimpar = () => {
+        setDadosFormulario(Object.fromEntries(Object.keys(dadosFormulario).map(campo => [campo, ''])));
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        try {
+            const resposta = await fetch('http://localhost:3333/clientes', {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dadosFormulario)
+            })
+
+            if (!resposta.ok) {
+                alert(`Erro ao cadastrar cliente, verifique se todos os dados estão sendo preenchidos de forma correta.`)
+                return
+            }
+
+            const dadosResposta = await resposta.json()
+            alert(`Cliente cadastrado com sucesso`)
+            handleLimpar()
+            dadosResposta
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return (
         <div className="container-novo">
             <h1 className="titulo">Crie sua conta</h1>
-            <form action="" className="form-container">
+            <form className="form-container" onSubmit={handleSubmit}>
                 <fieldset className="dados-pessoais">
                     <legend>Dados Pessoais</legend>
-                    <label htmlFor="">Nome</label>
-                    <input type="text" autoFocus/>
+                    <label htmlFor="nome">Nome</label>
+                    <input type="text" id="nome" value={dadosFormulario.name}
+                        onChange={(e) => handleChange("name", e.target.value)} autoFocus />
 
-                    <label htmlFor="">Sobrenome</label>
-                    <input type="text" />
+                    <label htmlFor="sobrenome">Sobrenome</label>
+                    <input type="text" id="sobrenome" value={dadosFormulario.surname}
+                        onChange={(e) => handleChange('surname', e.target.value)} />
 
-                    <label htmlFor="">Endereço</label>
-                    <input type="text" />
+                    <label htmlFor="endereco">Endereço</label>
+                    <input type="text" id="endereco" value={dadosFormulario.address}
+                        onChange={(e) => handleChange('address', e.target.value)} />
 
-                    <label htmlFor="">CPF</label>
-                    <input type="text" />
+                    <label htmlFor="cpf">CPF</label>
+                    <input type="text" id="cpf" value={dadosFormulario.cpf}
+                        onChange={(e) => handleChange('cpf', e.target.value)} />
 
-                    <label htmlFor="">Data de Nascimento</label>
-                    <input type="date" />
+                    <label htmlFor="dataNascimento">Data de Nascimento</label>
+                    <input type="date" id="dataNascimento" value={dadosFormulario.date}
+                        onChange={(e) => handleChange('date', e.target.value)} />
                 </fieldset>
                 <fieldset className="dados-pessoais">
                     <legend>Dados para login</legend>
-                    <label htmlFor="">Email</label>
-                    <input type="email" />
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" value={dadosFormulario.email}
+                        onChange={(e) => handleChange('email', e.target.value)} />
 
-                    <label htmlFor="">Senha</label>
-                    <input type="password" />
+                    <label htmlFor="senha">Senha</label>
+                    <input type="password" id="senha" value={dadosFormulario.senha}
+                        onChange={(e) => handleChange('senha', e.target.value)} />
                 </fieldset>
 
                 <fieldset className="form-btn">
